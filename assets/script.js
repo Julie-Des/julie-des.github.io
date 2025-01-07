38e5e860cc1b1fc2e8c56c12a6ff064c23dde7a2
Julie-Des <julie.deshayes14@gmail.com>
2025-01-07 09:14:36 +0100

    feat : add images ans dots with JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,62 +1,72 @@
 // Sélection des éléments du DOM
-const slides = document.querySelectorAll(".banner-img");
+const slide = document.querySelector(".banner-img");
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
-const dots = document.querySelectorAll(".dot");
+const dotsContainer = document.querySelector(".dots");
 const textContainer = document.querySelector("#banner p");
-const slidesText = [
-  "Impressions tous formats <span>en boutique et en ligne</span>",
-  "Qualité professionnelle <span>et service sur mesure</span>",
-  "Des tarifs compétitifs <span>et un service rapide</span>",
-  "Une équipe à votre écoute <span>pour tous vos projets</span>",
+const slidesData = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
 ];
 
+// Création des dots
+for (i = 0; i < slidesData.length; i++) {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  dotsContainer.appendChild(dot);
+}
 
 // Initialisation du carrousel
 let currentIndex = 0;
+displayCurrentSlide();
 
-for (let i = 1; i < slides.length; i++) {
-  slides[i].style.opacity = 0;
-}
+// Evénements au click
+arrowLeft.addEventListener("click", () => {
+  click(-1);
+});
+arrowRight.addEventListener("click", () => {
+  click(1);
+});
 
-// Evénements au click 
-arrowLeft.addEventListener("click", clickLeft);
-arrowRight.addEventListener("click", clickRight);
-
-
-function clickLeft() {
-  removeCurrentSlide();
-  moveIndexToLeft();
+function click(step) {
+  removeDotSelected();
+  moveIndex(step);
   displayCurrentSlide();
 }
 
-function clickRight() {
-  removeCurrentSlide();
-  moveIndexToRight();
-  displayCurrentSlide();
-}
-
-function removeCurrentSlide() {
-  dots[currentIndex].classList.remove("dot_selected");
-  slides[currentIndex].style.opacity = 0;
+function removeDotSelected() {
+  const dotSelected = document.querySelector(".dot_selected");
+  dotSelected.classList.remove("dot_selected");
 }
 
 function displayCurrentSlide() {
+  const dots = document.querySelectorAll(".dot");
   dots[currentIndex].classList.add("dot_selected");
-  slides[currentIndex].style.opacity = 1;
-  textContainer.innerHTML = slidesText[currentIndex];
+  slide.src = `./assets/images/slideshow/${slidesData[currentIndex].image}`;
+  textContainer.innerHTML = slidesData[currentIndex].tagLine;
 }
 
-function moveIndexToLeft() {
-  currentIndex -= 1;
-  if (currentIndex == -1) {
-    currentIndex = slides.length - 1;
+function moveIndex(step) {
+  currentIndex += step;
+  if (currentIndex === -1) {
+    currentIndex = slidesData.length - 1;
   }
-}
-
-function moveIndexToRight() {
-  currentIndex += 1;
-  if (currentIndex == slides.length) {
+  if (currentIndex === slidesData.length) {
     currentIndex = 0;
   }
 }
